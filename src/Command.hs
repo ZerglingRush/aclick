1507f20947083bb31f820b1a6ad4bc4334846eeb
chrisdotcode <chris@code.sc>
2014-04-05 16:29:10 -0400

    Add type signatures to functions.

--- src/Command.hs
+++ src/Command.hs
@@ -9,9 +9,6 @@ import Text.Printf
 import qualified Data.Map as Map
 import Data.Char (isDigit, toLower)
 
-port :: Int
-port = 6666
-
 properUsage :: Value
 properUsage = Error 1 "Use it like this 'set key value' asshole"
 
@@ -20,18 +17,14 @@ data Command = Set | Get | Incr
 data Value = IntValue Int | StringValue String | Error Int String
 
 instance Show Value where
-  show (IntValue i) = show i
+  show (IntValue i) = "(integer)" ++ show i
   show (StringValue s) = show s
-  show (Error i e) = show "(error)" ++ show i ++ show e
+  show (Error i e) = show "(error #)" ++ show i ++ ":" ++ show e
 
 parseValue :: String -> Value
 parseValue s
   | all isDigit s = IntValue $ read s
   | otherwise     = StringValue s
-
-valueToString :: Value -> String
-valueToString (IntValue v) = "(integer) " ++ show v
-valueToString (StringValue v) = show v
 
 fromString :: String -> Maybe Command
 fromString s
@@ -40,8 +33,13 @@ fromString s
   | map toLower s == "incr" = Just Incr
   | otherwise               = Nothing
 
+keyValue :: Map.Map String Value -> (Map.Map String Value, Value)
 keyValue (k:v:_) m = (Map.insert k (parseValue v) m, StringValue "success")
+
+getKey :: Map.Map String Value -> (Map.Map String Value, Value)
 getKey (k:_) m     = (m, m Map.! k)
+
+incrValue :: Map.Map String Value -> (Map.Map String Value, Value)
 incrValue (k:_) m  = (Map.insert k newVal m, newVal)
   where
     addOne (IntValue v) = IntValue (v + 1)
@@ -62,14 +60,13 @@ handleInput :: String -> Map.Map String Value
                -> IO (Map.Map String Value, Value)
 handleInput input r = return $ processCommands (words input) r
 
-
 handler :: Handle -> MVar (Map.Map String Value) -> IO ()
 handler h m = do
   hPutStr h ("Go to Hell!!!!!!\n")
   input <- (hGetLine h)
   print input
   result <- modifyMVar m (handleInput input)
-  hPutStr h ((valueToString result) ++ "\n")
+  hPutStr h ((show result) ++ "\n")
 
 listen :: Socket -> MVar (Map.Map String Value) -> IO ()
 listen sock m = do
