31021e865450da196236d3d4bc19e156481aa345
chrisdotcode <chris@code.sc>
2014-04-05 19:20:48 -0400

    Made Value a GADT.

--- src/Command.hs
+++ src/Command.hs
@@ -9,19 +9,22 @@ import Text.Printf
 import qualified Data.Map as Map
 import Data.Char (isDigit, toLower)
 
-invalidKey :: Value
+invalidKey :: Value a
 invalidKey = Error 2 "No key found"
 
 data Command = Set | Get | Incr
 
-data Value = IntValue Int | StringValue String | Error Int String
+data Value a where
+  IntValue :: Int -> Value a
+  StringValue :: String -> Value a
+  Error :: Int -> String -> Value a
 
-instance Show Value where
+instance Show (Value a) where
   show (IntValue i) = "(integer) " ++ show i
   show (StringValue s) = show s
   show (Error i e) = show "(error #)" ++ show i ++ ":" ++ show e
 
-parseValue :: String -> Value
+parseValue :: String -> Value a
 parseValue s
   | all isDigit s = IntValue $ read s
   | otherwise     = StringValue s
@@ -34,31 +37,31 @@ fromString s
   | otherwise               = Nothing
 
 getKey :: String ->
-          (Map.Map String Value) ->
-          (Map.Map String Value, Value)
+          (Map.Map String (Value a)) ->
+          (Map.Map String (Value a), Value a)
 getKey k m = (m, Map.findWithDefault invalidKey k m)
 
 setKey :: String ->
           String ->
-          (Map.Map String Value) ->
-          (Map.Map String Value, Value)
+          (Map.Map String (Value a)) ->
+          (Map.Map String (Value a), Value a)
 setKey k v m = (Map.insert k (parseValue v) m, StringValue "success")
 
 incrKey :: String ->
-           (Map.Map String Value) ->
-           (Map.Map String Value, Value)
+           (Map.Map String (Value a)) ->
+           (Map.Map String (Value a), Value a)
 incrKey k m = (Map.insert k newVal m, newVal)
   where
     addOne (IntValue v) = IntValue (v + 1)
     newVal = addOne $ Map.findWithDefault invalidKey k m
 
-invalidCommand :: Map.Map String Value -> (Map.Map String Value, Value)
+invalidCommand :: Map.Map String (Value a) -> (Map.Map String (Value a), Value a)
 invalidCommand m = (m, Error 1 "Use it like this 'set key value' asshole")
 
 processCommands
   :: [String]
-  -> Map.Map String Value
-  -> (Map.Map String Value, Value)
+  -> Map.Map String (Value a)
+  -> (Map.Map String (Value a), Value a)
 processCommands [] m = invalidCommand m
 processCommands [x] m = invalidCommand m
 processCommands (x:y:[]) m = case (fromString x) of
@@ -69,11 +72,11 @@ processCommands (x:y:z:[]) m = case (fromString x) of
   Just Set -> setKey y z m
   Nothing  -> invalidCommand m
 
-handleInput :: String -> Map.Map String Value
-               -> IO (Map.Map String Value, Value)
+handleInput :: String -> Map.Map String (Value a)
+               -> IO (Map.Map String (Value a), Value a)
 handleInput input r = return $ processCommands (words input) r
 
-handler :: Handle -> MVar (Map.Map String Value) -> IO ()
+handler :: Handle -> MVar (Map.Map String (Value a)) -> IO ()
 handler h m = do
   hPutStr h ("Go to Hell!!!!!!\n")
   input <- (hGetLine h)
@@ -81,7 +84,7 @@ handler h m = do
   result <- modifyMVar m (handleInput input)
   hPutStr h ((show result) ++ "\n")
 
-listen :: Socket -> MVar (Map.Map String Value) -> IO ()
+listen :: Socket -> MVar (Map.Map String (Value a)) -> IO ()
 listen sock m = do
   (handle, _, _) <- accept sock
   _ <- forkFinally (handler handle m) (\_ -> hClose handle)
