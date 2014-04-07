c321006eafa09a50fadc11bc94959bbf77ab940b
chrisdotcode <chris@code.sc>
2014-04-06 23:48:01 -0400

    Create Database type.

--- src/Command.hs
+++ src/Command.hs
@@ -11,15 +11,11 @@ import Text.Printf
 import qualified Data.Map as Map
 import Data.Char (isDigit, toLower)
 
-invalidCommand :: Map.Map String Value -> (Map.Map String Value, Value)
-invalidCommand m = (m, Error 1 "Invalid command")
-
-invalidKey :: Value
-invalidKey = Error 2 "No key found"
-
 data Command = Set | Get | Incr
 
 data Value = IntValue Int | StringValue String | Error Int String
+
+type Database = Map.Map String Value
 
 instance Show Value where
   show (IntValue i) = "(integer) " ++ show i
@@ -38,23 +34,25 @@ fromString s
   | map toLower s == "incr" = Just Incr
   | otherwise               = Nothing
 
-getKey :: String -> (Map.Map String Value) -> (Map.Map String Value, Value)
+invalidCommand :: Database -> (Database, Value)
+invalidCommand m = (m, Error 1 "Invalid command")
+
+invalidKey :: Value
+invalidKey = Error 2 "No key found"
+
+getKey :: String -> Database -> (Database, Value)
 getKey k m = (m, Map.findWithDefault invalidKey k m)
 
-setKey :: String -> String ->
-          (Map.Map String Value) ->
-          (Map.Map String Value, Value)
+setKey :: String -> String -> Database -> (Database, Value)
 setKey k v m = (Map.insert k (parseValue v) m, StringValue "success")
 
-incrKey :: String -> (Map.Map String Value) -> (Map.Map String Value, Value)
+incrKey :: String -> Database -> (Database, Value)
 incrKey k m = (Map.insert k newVal m, newVal)
   where
     addOne (IntValue v) = IntValue (v + 1)
     newVal = addOne $ Map.findWithDefault invalidKey k m
 
-processCommands :: [String]
-  -> Map.Map String Value
-  -> (Map.Map String Value, Value)
+processCommands :: [String] -> Database -> (Database, Value)
 processCommands [] m = invalidCommand m
 processCommands [x] m = invalidCommand m
 processCommands (x:y:[]) m = case (fromString x) of
@@ -65,14 +63,14 @@ processCommands (x:y:z:[]) m = case (fromString x) of
   Just Set -> setKey y z m
   Nothing  -> invalidCommand m
 
-handleInput :: String -> TVar (Map.Map String Value) -> STM Value
+handleInput :: String -> TVar Database -> STM Value
 handleInput input m = do
   table <- readTVar m
   let (newTable, result) = processCommands (words input) table
   writeTVar m newTable
   return result
 
-handler :: Handle -> TVar (Map.Map String Value) -> IO ()
+handler :: Handle -> TVar Database -> IO ()
 handler h m = do
   hPutStrLn h ("Aclick ver 0")
   input <- (hGetLine h)
@@ -80,7 +78,7 @@ handler h m = do
   result <- atomically $ (handleInput input m)
   hPutStr h ((show result) ++ "\n")
 
-listen :: Socket -> TVar (Map.Map String Value) -> IO ()
+listen :: Socket -> TVar Database -> IO ()
 listen sock m = do
   (handle, _, _) <- accept sock
   _ <- forkFinally (handler handle m) (\_ -> hClose handle)
