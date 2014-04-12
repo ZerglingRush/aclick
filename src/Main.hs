a59fd5110a1d88ab6b79c273fbdd7e15a5914eec
chrisdotcode <chris@code.sc>
2014-04-11 22:36:20 -0400

    Make imports explicit in Main.hs.

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,6 @@
 module Main where
 
-import Control.Concurrent.STM
-import Control.Monad
-import Network
-import System.IO
-import Text.Printf
-import qualified Data.Map as Map
-import Data.Char (isDigit, toLower)
-
 import Server
-import Core
 
 main :: IO ()
 main = server
